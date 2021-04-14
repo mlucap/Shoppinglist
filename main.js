a074955d1ec72badd975555331644a9e4cc97672
Luca <pereluca10@gmail.com>
2021-04-13 20:41:19 -0400

    Basically finished, added some styling with bulma

--- main.js
+++ main.js
@@ -2,27 +2,70 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
+let addWindow;
 
 // Listen for app to be ready
-app.on('ready', function () {
+app.on('ready', () => {
 	// create new window
-	mainWindow = new BrowserWindow({});
+	mainWindow = new BrowserWindow({
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+		},
+	});
 
 	mainWindow.loadURL(
 		url.format({
-			pathname: path.join(__dirname, 'mainWindow.html'),
+			pathname: path.join(__dirname, 'src/mainWindow.html'),
 			protocol: 'file',
 			slashes: true,
 		})
 	);
+	// Quit app when closed
+	mainWindow.on('closed', () => {
+		app.quit();
+	});
 
 	// Build menu from template
 	const mainMenu = Menu.buildFromTemplate(menuTemplate);
 	// Insert menu
 	Menu.setApplicationMenu(mainMenu);
+});
+
+// Handle create add window
+function createAddWindow() {
+	// create new window
+	addWindow = new BrowserWindow({
+		width: 300,
+		height: 200,
+		title: 'Add item',
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+		},
+	});
+
+	addWindow.loadURL(
+		url.format({
+			pathname: path.join(__dirname, 'src/addWindow.html'),
+			protocol: 'file',
+			slashes: true,
+		})
+	);
+	// Garbage collection
+	addWindow.on('closed', () => {
+		addWindow = null;
+	});
+}
+
+// Catch ipc item:add
+ipcMain.on('item:add', (e, item) => {
+	console.log(item);
+	mainWindow.webContents.send('item:add', item);
+	addWindow.close();
 });
 
 const menuTemplate = [
@@ -31,9 +74,15 @@ const menuTemplate = [
 		submenu: [
 			{
 				label: 'Add Item',
+				click() {
+					createAddWindow();
+				},
 			},
 			{
 				label: 'Clear Items',
+				click() {
+					mainWindow.webContents.send('item:clear');
+				},
 			},
 			{
 				label: 'Quit',
@@ -45,3 +94,22 @@ const menuTemplate = [
 		],
 	},
 ];
+
+// Add dev tools if not in prod
+if (process.env.NODE_ENV !== 'production') {
+	menuTemplate.push({
+		label: 'Dev tools',
+		submenu: [
+			{
+				label: 'Toggle Dev tools',
+				accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+				click(item, focusedWindow) {
+					focusedWindow.toggleDevTools();
+				},
+			},
+			{
+				role: 'reload',
+			},
+		],
+	});
+}
